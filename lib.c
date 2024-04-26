5fcf8459366cfef5a8d21dd1c56316597ee2fd47
galeanoyapio <166860602+galeanoyapio@users.noreply.github.com>
2024-04-26 15:54:53 -0300

    lib.c

--- lib.c
+++ lib.c
@@ -2,22 +2,28 @@
 #include "MKL25Z4.h"
 
 void pinIO(int pin, char IO){
-    if(IO=='i'){
-        PORTA -> PCR[pin] |= PORT_PCR_MUX(0);
-    }elseif(IO=='o'){
+    PORTA -> PCR[pin] |= PORT_PCR_MUX (IO);
+     if(IO=='i'){
+         PORTA -> PCR[pin] |= PORT_PCR_MUX(0);
+    }else if(IO=='o'){
         PORTA -> PCR[pin] |= PORT_PCR_MUX(1);
-    }
+     }
+
 }
 int estado (int pin){
     int estado = PTA -> PDIR & (1u<<pin);
     return estado;
 
 }
-char pinAoI (int pin){
+char pinAoI(int pin){
     if(estado(pin)==1){
         return 'A';
     }else if (estado(pin)==0){
         return 'I';
     
 }
+}
+int establecer(int pin, int est){
+        PTA -> PSOR |= (est<<pin);
+    
 }
